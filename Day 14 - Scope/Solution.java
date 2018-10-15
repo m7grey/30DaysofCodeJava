04028d13bc40009b2019221c4ed02686122b5bfa
Serhiy Yarosh <m7grey@gmail.com>
2018-10-15 14:02:49 -0400

    Initial commit - Solved Solutions.

---
+++ Day 14 - Scope/Solution.java
@@ -0,0 +1,40 @@
+import java.io.*;
+import java.math.*;
+import java.text.*;
+import java.util.*;
+import java.util.regex.*;
+
+class Difference {
+    public int maximumDifference;
+    private int[] elements;
+
+    Difference(int[] elements) {
+        this.elements = elements;
+    }
+
+    //          Runtime: O(n)
+    // Space Complexity: O(1)
+    void computeDifference() {
+        int max = Arrays.stream(elements).max().getAsInt();
+        int min = Arrays.stream(elements).min().getAsInt();
+        maximumDifference = max - min;
+    }
+} // End of Difference class
+
+public class Solution {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] a = new int[n];
+        for (int i = 0; i < n; i++) {
+            a[i] = sc.nextInt();
+        }
+        sc.close();
+
+        Difference difference = new Difference(a);
+
+        difference.computeDifference();
+
+        System.out.print(difference.maximumDifference);
+    }
+}
---
+++ readme.md
@@ -0,0 +1,33 @@
+# 30 Days of Code Java
+HackerRank 30 Days of Code Challenges (2018) written in Java 8.
+
+- [x] Day 0: Hello World
+- [x] Day 1: Data Types
+- [x] Day 2: Operators
+- [x] Day 3: Intro to Conditional Statements
+- [x] Day 4: Class vs. Instance
+- [x] Day 5: Loops
+- [x] Day 6: Let's Review
+- [x] Day 7: Arrays
+- [x] Day 8: Dictionaries and Maps
+- [x] Day 9: Recursion
+- [x] Day 10: Binary Numbers
+- [x] Day 11: 2D Arrays
+- [x] Day 12: Inheritance
+- [x] Day 13: Abstract Classes
+- [x] Day 14: Scope
+- [x] Day 15: Linked List
+- [x] Day 16: Exceptions - String to Integer
+- [x] Day 17: More Exceptions
+- [x] Day 18: Queues and Stacks
+- [x] Day 19: Interfaces
+- [x] Day 20: Sorting
+- [x] Day 21: Generics
+- [x] Day 22: Binary Search Trees
+- [x] Day 23: BST Level-Order Traversal
+- [x] Day 24: More Linked Lists
+- [x] Day 25: Running Time and Complexity
+- [x] Day 26: Nested Logic
+- [x] Day 27: Testing
+- [x] Day 28: RegEx, Patterns, and Intro to Databases
+- [x] Day 29: Bitwise AND
